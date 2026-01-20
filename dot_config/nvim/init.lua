907fbac1c4df4be177381b7ecb6b6ee0ac5cef07
timngmsft <timngmsft@hotmail.com>
2026-01-19 19:22:37 -0800

    Support for NeoVim

---
+++ dot_config/nvim/init.lua
@@ -0,0 +1,22 @@
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Load plugins
+require("lazy").setup("plugins")
+
+-- Load colorscheme
+vim.cmd("colorscheme gruvbox")
+
+-- Source vim settings
+vim.cmd("source ~/tim.nvim.settings.vim")
---
+++ dot_config/nvim/lua/plugins.lua
@@ -0,0 +1,78 @@
+return {
+  -- Colorschemes
+  { "altercation/vim-colors-solarized" },
+  { "morhetz/gruvbox" },
+  { "tomasr/molokai" },
+
+  -- UI
+  { "nathanaelkane/vim-indent-guides" },
+  {
+    "vim-airline/vim-airline",
+    dependencies = { "vim-airline/vim-airline-themes" },
+  },
+  { "bling/vim-bufferline" },
+
+  -- Editing
+  { "tpope/vim-surround" },
+  { "easymotion/vim-easymotion" },
+  { "scrooloose/nerdcommenter" },
+
+  -- Git
+  { "airblade/vim-gitgutter" },
+
+  -- Tags
+  { "xolox/vim-misc" },
+  { "majutsushi/tagbar" },
+
+  -- CodeCompanion dependencies
+  { "nvim-lua/plenary.nvim" },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      -- Neovim 0.11+ has built-in treesitter highlight, just install parsers
+      vim.api.nvim_create_autocmd("FileType", {
+        callback = function()
+          pcall(vim.treesitter.start)
+        end,
+      })
+    end,
+  },
+
+  -- CodeCompanion
+  {
+    "olimorris/codecompanion.nvim",
+    version = "^18.0.0",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    config = function()
+      require("codecompanion").setup({
+        adapters = {
+          azure_openai = function()
+            return require("codecompanion.adapters").extend("azure_openai", {
+              env = {
+                api_key = "AZURE_OPENAI_API_KEY",
+                endpoint = "AZURE_OPENAI_ENDPOINT",
+              },
+              schema = {
+                model = {
+                  default = "<your-deployment-name>",
+                },
+              },
+            })
+          end,
+        },
+        strategies = {
+          chat = {
+            adapter = "azure_openai",
+          },
+          inline = {
+            adapter = "azure_openai",
+          },
+        },
+      })
+    end,
+  },
+}
